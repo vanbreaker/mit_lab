001cb15db5efa0e4fe93b849501cfe69952f0b97
taosheng <taosheng@xunlei.com>
2018-10-19 16:41:22 +0800

    commit mapreduce lab PART3

--- 6.824-golabs-2018/src/mapreduce/schedule.go
+++ 6.824-golabs-2018/src/mapreduce/schedule.go
@@ -5,13 +5,11 @@ import (
 	"sync"
 )
 
-type WorkersMgr struct {
+type workersManager struct {
 	sync.Mutex
-	workers []string
-	nFree   int
+	regWorkers  []string
+	freeWorkers []string
 }
-
-var freeWorkers []string
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -41,15 +39,22 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// Your code here (Part III, Part IV).
 	//
+
+	var workersMgr workersManager
 	var wg sync.WaitGroup
 	nLeftTasks := ntasks
+	freeChan := make(chan string)
+
+	go recvRegWorker(&workersMgr, registerChan, freeChan)
+
 	for nLeftTasks > 0 {
-		freeCnt := len(freeWorkers)
-		if l > 0 {
-			worker := freeWorkers[freeCnt-1]
-			freeWorkers = freeWorkers[:freeCnt-1]
+		freeCnt := len(workersMgr.freeWorkers)
+		//fmt.Printf("free workers %d : %v\n", freeCnt, workersMgr.freeWorkers)
+		if freeCnt > 0 {
+			worker := workersMgr.freeWorkers[freeCnt-1]
+			workersMgr.freeWorkers = workersMgr.freeWorkers[:freeCnt-1]
 			taskArg := DoTaskArgs{
-				jobName:       jobName,
+				JobName:       jobName,
 				Phase:         phase,
 				TaskNumber:    ntasks - nLeftTasks,
 				NumOtherPhase: n_other,
@@ -57,23 +62,54 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 			if phase == "mapPhase" {
 				taskArg.File = mapFiles[ntasks-nLeftTasks]
 			}
-
-			go submitTask(worker, taskArg, registerChan, wg)
-
-		} else {
-
+			wg.Add(1)
+			go submitTask(worker, taskArg, freeChan, &wg)
+			nLeftTasks--
+		} else { // no free worker, wait for new registered worker or job done worker
+			//		fmt.Printf("wait for free worker\n")
+			fw := <-freeChan
+			//		fmt.Printf("recv free worker:%s\n", fw)
+			workersMgr.freeWorkers = append(workersMgr.freeWorkers, fw)
+			fmt.Printf("free workers:%v\n", workersMgr.freeWorkers)
 		}
 	}
 
+	for {
+		workersMgr.Lock()
+		regCnt := len(workersMgr.regWorkers)
+		workersMgr.Unlock()
+		freeCnt := len(workersMgr.freeWorkers)
+		if freeCnt < regCnt {
+			fw := <-freeChan
+			workersMgr.freeWorkers = append(workersMgr.freeWorkers, fw)
+		} else {
+			break
+		}
+	}
+
+	wg.Wait() // wait for all tasks
 	fmt.Printf("Schedule: %v done\n", phase)
 }
 
-func submitTask(worker string, arg DoTaskArgs, doneChan chan string, wg *sync.WaitGroup) {
-	wg.Add(1)
+func recvRegWorker(workersMgr *workersManager, registerChan chan string, freeChan chan string) {
+	for {
+		worker := <-registerChan
+		workersMgr.Lock()
+		workersMgr.regWorkers = append(workersMgr.regWorkers, worker)
+		workersMgr.Unlock()
+		//fmt.Printf("register new worker:%s\n", worker)
+		freeChan <- worker
+	}
+}
+
+func submitTask(worker string, arg DoTaskArgs, freeChan chan string, wg *sync.WaitGroup) {
+	//fmt.Printf("dispatch task %d to worker %s\n", arg.TaskNumber, worker)
+	defer wg.Done()
 	ok := call(worker, "Worker.DoTask", arg, new(struct{}))
 	if ok == false {
 		fmt.Printf("Master: RPC %s doTask error\n", worker)
 	} else {
-		doneChan <- worker
+		//fmt.Printf("free worker:%s\n", worker)
+		freeChan <- worker
 	}
 }
