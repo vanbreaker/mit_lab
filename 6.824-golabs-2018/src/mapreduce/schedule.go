f7a774051653fbbc31250ce4d0b6f9695a57590b
taosheng <taosheng@xunlei.com>
2018-10-22 11:47:08 +0800

    commit mapreduce PART 4

--- 6.824-golabs-2018/src/mapreduce/schedule.go
+++ 6.824-golabs-2018/src/mapreduce/schedule.go
@@ -7,8 +7,16 @@ import (
 
 type workersManager struct {
 	sync.Mutex
-	regWorkers  []string
-	freeWorkers []string
+	regWorkers    []string
+	freeWorkers   []string
+	failedWorkers []string
+}
+
+type workerNotify struct {
+	workerName     string
+	isNewRegWorker bool
+	taskResult     bool
+	taskArg        DoTaskArgs
 }
 
 //
@@ -42,35 +50,59 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 
 	var workersMgr workersManager
 	var wg sync.WaitGroup
-	nLeftTasks := ntasks
-	freeChan := make(chan string)
+	freeChan := make(chan workerNotify)
+
+	var unsubmitTaskList []DoTaskArgs              //tasks wait to submit
+	handlingTaskMap := make(map[string]DoTaskArgs) //tasks handling by workers
+
+	//initialize unsubmit task list
+	for i := ntasks; i > 0; i-- {
+		taskArg := DoTaskArgs{
+			JobName:       jobName,
+			Phase:         phase,
+			TaskNumber:    i - 1,
+			NumOtherPhase: n_other,
+		}
+		if phase == "mapPhase" {
+			taskArg.File = mapFiles[i-1]
+		}
+		unsubmitTaskList = append(unsubmitTaskList, taskArg)
+	}
 
 	go recvRegWorker(&workersMgr, registerChan, freeChan)
 
-	for nLeftTasks > 0 {
+	for len(unsubmitTaskList) > 0 || len(handlingTaskMap) > 0 {
 		freeCnt := len(workersMgr.freeWorkers)
 		//fmt.Printf("free workers %d : %v\n", freeCnt, workersMgr.freeWorkers)
 		if freeCnt > 0 {
+			//fetch a worker from free workers
 			worker := workersMgr.freeWorkers[freeCnt-1]
 			workersMgr.freeWorkers = workersMgr.freeWorkers[:freeCnt-1]
-			taskArg := DoTaskArgs{
-				JobName:       jobName,
-				Phase:         phase,
-				TaskNumber:    ntasks - nLeftTasks,
-				NumOtherPhase: n_other,
-			}
-			if phase == "mapPhase" {
-				taskArg.File = mapFiles[ntasks-nLeftTasks]
-			}
+
+			//move an unsubmit task to handling task collection
+			taskArg := unsubmitTaskList[len(unsubmitTaskList)-1]
+			unsubmitTaskList = unsubmitTaskList[:len(unsubmitTaskList)-1]
+			handlingTaskMap[taskArg.JobName] = taskArg
 			wg.Add(1)
 			go submitTask(worker, taskArg, freeChan, &wg)
-			nLeftTasks--
 		} else { // no free worker, wait for new registered worker or job done worker
-			//		fmt.Printf("wait for free worker\n")
-			fw := <-freeChan
-			//		fmt.Printf("recv free worker:%s\n", fw)
-			workersMgr.freeWorkers = append(workersMgr.freeWorkers, fw)
-			fmt.Printf("free workers:%v\n", workersMgr.freeWorkers)
+			notify := <-freeChan
+
+			//recv new registered worker
+			if notify.isNewRegWorker {
+				workersMgr.freeWorkers = append(workersMgr.freeWorkers, notify.workerName)
+				continue
+			}
+
+			if notify.taskResult == true { //worker successfully done the job
+				workersMgr.freeWorkers = append(workersMgr.freeWorkers, notify.workerName)
+			} else { //worker failed, put the task back to unsubmit task list and drop the worker
+				fmt.Printf("worker %s FAILED when doing task %s\n", notify.workerName, notify.taskArg.JobName)
+				workersMgr.failedWorkers = append(workersMgr.failedWorkers, notify.workerName)
+				unsubmitTaskList = append(unsubmitTaskList, notify.taskArg)
+			}
+
+			delete(handlingTaskMap, notify.taskArg.JobName)
 		}
 	}
 
@@ -79,9 +111,10 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 		regCnt := len(workersMgr.regWorkers)
 		workersMgr.Unlock()
 		freeCnt := len(workersMgr.freeWorkers)
-		if freeCnt < regCnt {
+		failedCnt := len(workersMgr.failedWorkers)
+		if freeCnt+failedCnt < regCnt {
 			fw := <-freeChan
-			workersMgr.freeWorkers = append(workersMgr.freeWorkers, fw)
+			workersMgr.freeWorkers = append(workersMgr.freeWorkers, fw.workerName)
 		} else {
 			break
 		}
@@ -91,25 +124,35 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	fmt.Printf("Schedule: %v done\n", phase)
 }
 
-func recvRegWorker(workersMgr *workersManager, registerChan chan string, freeChan chan string) {
+func recvRegWorker(workersMgr *workersManager, registerChan chan string, freeChan chan workerNotify) {
 	for {
 		worker := <-registerChan
 		workersMgr.Lock()
 		workersMgr.regWorkers = append(workersMgr.regWorkers, worker)
 		workersMgr.Unlock()
 		//fmt.Printf("register new worker:%s\n", worker)
-		freeChan <- worker
+		notify := workerNotify{
+			workerName:     worker,
+			isNewRegWorker: true,
+		}
+		freeChan <- notify
 	}
 }
 
-func submitTask(worker string, arg DoTaskArgs, freeChan chan string, wg *sync.WaitGroup) {
-	//fmt.Printf("dispatch task %d to worker %s\n", arg.TaskNumber, worker)
+func submitTask(worker string, arg DoTaskArgs, freeChan chan workerNotify, wg *sync.WaitGroup) {
+	fmt.Printf("dispatch task %d to worker %s\n", arg.TaskNumber, worker)
 	defer wg.Done()
 	ok := call(worker, "Worker.DoTask", arg, new(struct{}))
 	if ok == false {
 		fmt.Printf("Master: RPC %s doTask error\n", worker)
-	} else {
-		//fmt.Printf("free worker:%s\n", worker)
-		freeChan <- worker
 	}
+
+	notify := workerNotify{
+		workerName:     worker,
+		isNewRegWorker: false,
+		taskResult:     ok,
+		taskArg:        arg,
+	}
+
+	freeChan <- notify
 }
